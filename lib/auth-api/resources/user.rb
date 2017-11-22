621a30b8bd2d5713d4c14d92eebcf8a56059c5e5
Jeremy Jackson <jejacks0n@gmail.com>
2017-11-22 11:59:32 -0700

    adds full_name to user resource.

--- lib/auth-api/resources/user.rb
+++ lib/auth-api/resources/user.rb
@@ -1,5 +1,8 @@
 module AuthApi
   class User < Resource::Base
     define_array_attributes :products, :registrations, :roles
+    def full_name
+      [first_name, last_name].compact.join(' ')
+    end
   end
 end
