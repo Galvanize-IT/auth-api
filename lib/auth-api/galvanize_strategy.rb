bfd3a5a462c32619aa8b55ff6998eae346db6660
Andrew Singley <andrew.singley@galvanize.com>
2024-01-05 17:31:40 -0500

    update strategy with site option including url

--- lib/auth-api/galvanize_strategy.rb
+++ lib/auth-api/galvanize_strategy.rb
@@ -1,14 +1,34 @@
 module OmniAuth
   module Strategies
     class Galvanize < OmniAuth::Strategies::OAuth2
+      # Give your strategy a name.
       option :name, :galvanize
 
+      # This is where you pass the options you would pass when
+      # initializing your consumer from the OAuth gem.
+      option :client_options, {:site => "http://localhost:3003"}
+
+      # You may specify that your strategy should use PKCE by setting
+      # the pkce option to true: https://tools.ietf.org/html/rfc7636
+      option :pkce, true
+
+      # These are called after authentication has succeeded. If
+      # possible, you should try to set the UID without making
+      # additional calls (if the user id is returned with the token
+      # or as a URI parameter). This may not be possible with all
+      # providers.
       uid do
         raw_info["data"]["attributes"]["uid"]
       end
 
       info do
         raw_info
+      end
+
+      extra do
+        {
+          'raw_info' => raw_info
+        }
       end
 
       def raw_info
